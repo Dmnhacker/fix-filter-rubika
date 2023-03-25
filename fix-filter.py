37619355d48b949e260151b99621ddf4bf3b73b9
DMN <127993969+Dmnhacker@users.noreply.github.com>
2023-03-26 01:27:22 +0330

    Create fix-filter.py

---
+++ fix-filter.py
@@ -0,0 +1,93 @@
+import time
+import os
+from os import system
+from time import sleep
+system("clear")
+os.system("")
+class color:
+    green = "\033[32m"
+    red = "\033[31m"
+    blue = "\033[36m"
+    pink = "\033[35m"
+    yellow = "\033[93m"
+    darkblue = "\033[34m"
+    white = "\033[00m"
+    mark = '\x1b[38;5;4m'
+    mark1 = '\x1b[48;5;15m'
+    mark2 = '\x1b[0m'
+print(color.mark+f"INSTALLING",end='')
+sleep(1)
+print(color.green+f".",end='',flush=True)
+sleep(1)
+print(color.green+f".",end='',flush=True)
+sleep(1)
+print(color.green+f".",end='',flush=True)
+sleep(1)
+print(color.green+f".",end='',flush=True)
+sleep(1)
+print(color.green+f".",end='',flush=True)
+sleep(1)
+print(color.green+f".",end='',flush=True)
+sleep(1)
+print(color.green+f".",end='',flush=True)
+sleep(1)
+print(color.yellow+f"    
+      
+      FIX FILTER SCRIPT! ",end='',flush=True)
+sleep(1)
+p="""
+
+Enter ID FILTER ACCOUNT BUT NOT (@)
+
+
+OK?
+
+for i in p:
+    sleep(0.05)
+    print(color.pink+i,end='',flush=True)
+Ent=input(color.pink+'Enter your ID ACCOUNT FOR FIX>>> ')
+
+
+code1="""fix.filter.rubika.ir(rubika.ir/"""+Ent+""")This account was for a child's study and was filtered by hackers please delete his accountPlease fix filter this account"""
+code2="""order=fix.filter>rubika.ir(rubika.ir/"""+Ent+""")This account has not been violated, remove it from the filter"""
+code3="""liberation.account.in.filter/sport(rubika.ir/"""+Ent+""")This account has a religious owner who is the admin of a channel and the hackers have filtered it remove it from the filter"""
+code4="""Getting.out.of.suspension.spport.bot(rubika.ir/"""+Ent+""") """
+
+print(color.green+f" code1 ",end='',flush=True)
+sleep(1)
+
+for s in code1 :
+    sleep(0.03)
+    print(color.mark+s,end='',flush=True)
+print(color.mark2")
+
+print(color.blue+f" code2 ",end='',flush=True)
+sleep(1)
+
+for s in code2 :
+    sleep(0.03)
+    print(color.mark1+s,end='',flush=True)
+print(color.red")
+
+print(color.red+f" code3 ",end='',flush=True)
+sleep(1)
+
+for s in code3 :
+    sleep(0.03)
+    print(color.mark+s,end='',flush=True)
+print(color.mark2")
+
+print(color.yellow+f" code4 ",end='',flush=True)
+sleep(1)
+
+for s in code4:
+    sleep(0.03)
+    print(color.mark1+s,end='',flush=True)
+print(color.pink")
+
+
+exit1=input(color.green+"Enter key For back >>> ")
+print('ok')
+time.sleep(2)
+system("clear")
+time.sleep(2)
