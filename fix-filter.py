a8c509e2c5ad22a035ebba7dab37aa1dd3560f58
DMN <127993969+Dmnhacker@users.noreply.github.com>
2023-03-26 15:56:15 +0330

    Update fix-filter.py

--- fix-filter.py
+++ fix-filter.py
@@ -64,23 +64,23 @@ sleep(1)
 for s in code1:
     sleep(0.03)
     print(color.mark+s,end='',flush=True)
-print(color.mark2+":)")
+    print(color.mark2+":)")
 
-print(color.blue+f"code2 ",end='',flush=True)
+    print(color.blue+f"code2 ",end='',flush=True)
 sleep(1)
 
 for s in code2:
     sleep(0.03)
     print(color.mark1+s,end='',flush=True)
-print(color.mark2+":)")
+    print(color.mark2+":)")
 
-print(color.red+f"code3 ",end='',flush=True)
+    print(color.red+f"code3 ",end='',flush=True)
 sleep(1)
 
 for s in code3:
     sleep(0.03)
     print(color.mark+s,end='',flush=True)
-print(color.mark2+":)")
+    print(color.mark2+":)")
 
     print(color.blue+f"code4 ",end='',flush=True)
 sleep(1)
@@ -88,7 +88,7 @@ sleep(1)
 for s in code4:
     sleep(0.03)
     print(color.mark+s,end='',flush=True)
-print(color.mark2+":)")
+    print(color.mark2+":)")
       
 exit1=input(color.green+"Enter key For back >>> ")
 print('ok')
