54ffdc12df59aa3a56f1bb18fcbfc30e7a9e2fd3
DMN <127993969+Dmnhacker@users.noreply.github.com>
2023-03-26 16:19:17 +0330

    Update fix-filter.py

--- fix-filter.py
+++ fix-filter.py
@@ -62,7 +62,7 @@ Ent=input(color.pink+'Enter your ID ACCOUNT FOR FIX>>> ')
 code1 = "fix.filter.rubika.ir(rubika.ir/"""+Ent+""")This account was for a child's study and was filtered by hackers please delete his account Please fix filter this account"
 code2 = "order=fix.filter>rubika.ir(rubika.ir/"""+Ent+""")This account has not been violated, remove it from the filter"
 code3 = "liberation.account.in.filter/sport(rubika.ir/"""+Ent+""")This account has a religious owner who is the admin of a channel and the hackers have filtered it remove it from the filter"
-code4 = """"Getting.out.of.suspension.spport.bot(rubika.ir/"""+Ent+""")This account was hacked and the violations were committed in the hack mode and the real admin did not do this please remove it from filter""""
+code4 = ""Getting.out.of.suspension.spport.bot(rubika.ir/"""+Ent+""")This account was hacked and the violations were committed in the hack mode and the real admin did not do this please remove it from filter""
 
 print(color.green+f"code1 ",end='',flush=True)
 sleep(1)
