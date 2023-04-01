50669741ff13cbcc5ff5800767f0761725a41366
DMN <127993969+Dmnhacker@users.noreply.github.com>
2023-04-01 14:59:23 +0330

    Create code-fix-mini.py

---
+++ code-fix-mini.py
@@ -0,0 +1,37 @@
+import random
+import time
+import os
+from os import system
+from time import sleep
+import sys
+import datetime
+import requests
+system("clear")
+os.system("")
+class color:
+    green = "\033[32m"
+    red = "\033[31m"
+    blue = "\033[36m"
+    pink = "\033[35m"
+    yellow = "\033[93m"
+    darkblue = "\033[34m"
+    white = "\033[00m"
+    mark = '\x1b[38;5;4m'
+    mark1 = '\x1b[48;5;15m'
+    mark2 = '\x1b[0m'
+print(color.mark+f"WELCOME",end='')
+sleep(1)
+print(f"\033[34m")
+coderize = random.choice(repoestsrub)
+repoestsrub = ['Rubika.ir/raf-filter','Rubika.ir/fixing-filter','Rubika.ir/Account-liberation','Rubika.ir/rf-fil-rubuk','Rubika.ir/Please-unfilter-this-account','Rubika.ir/plase-fix-filter-this-account','Rubika.ir/fix-fix-fil','Rubika.ir/fixing-fil','Rubika.ir/moslim-account','Rubika.ir/Religious-account','Rubika.ir/Support-fix-filter','Rubika.ir/This-Account-Undertone-Do-Not','Rubika.ir/This-Account-Mistake-Filtered','Rubika.ir/This-Account-must-fix-Filter','Rubika.ir/Bring-this-account-from-the-sanctuary','Rubika.ir/rf-filtering','Rubika.ir/raf-filter','Rubika.ir/fixing-filter','Rubika.ir/Account-liberation','Rubika.ir/rf-fil-rubuk','Rubika.ir/Please-unfilter-this-account','Rubika.ir/plase-fix-filter-this-account','Rubika.ir/fix-fix-fil','Rubika.ir/fixing-fil','Rubika.ir/moslim-account','Rubika.ir/Religious-account','Rubika.ir/Support-fix-filter','Rubika.ir/This-Account-Undertone-Do-Not','Rubika.ir/This-Account-Mistake-Filtered','Rubika.ir/This-Account-must-fix-Filter','Rubika.ir/Bring-this-account-from-the-sanctuary','Rubika.ir/rf-filtering']
+
+y0y =f"""
+
+for give code inter coderize 
+"""
+
+
+
+for i in y0y:
+    sleep(0.05)
+    print(color.pink+i,end='',flush=True)
